20a8cfb30638351a633e17e21994972e5d8e28d8
dauglas <tang85718@users.noreply.github.com>
2018-10-08 21:30:04 +0800

    Initial commit

---
+++ src/main/java/co/story/server/EasyStoryServerApplication.java
@@ -0,0 +1,12 @@
+package co.story.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyStoryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyStoryServerApplication.class, args);
+    }
+}
